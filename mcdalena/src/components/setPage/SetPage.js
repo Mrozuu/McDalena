e9f4355ada93ca63c788614cc6435826da320bb1
kalllllor <karolgren3@gmail.com>
2020-03-29 14:49:30 +0200

    KG #8

--- mcdalena/src/components/setPage/SetPage.js
+++ mcdalena/src/components/setPage/SetPage.js
@@ -16,6 +16,7 @@ class SetPage extends React.Component {
     return (
       <div className="setPage">
         <MainLogo />
+        <SetTopBar />
         <SetPicture />
         <SetContent />
         <LittleLogo />
