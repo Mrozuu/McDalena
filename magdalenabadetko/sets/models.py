96ec84639c773acf8ed36f923b2e04fa46941be0
Mrozuu <58520713+Mrozuu@users.noreply.github.com>
2020-04-18 21:15:43 +0200

    MJ #29

--- magdalenabadetko/sets/models.py
+++ magdalenabadetko/sets/models.py
@@ -4,14 +4,21 @@ from django.db import models
 
 class Set(models.Model):
 
-    title = models.CharField(max_length=200)
-    description = models.CharField(max_length=1000)
-    list_of_recipes = ArrayField(
-        models.CharField(max_length=200),
-        size=20
-    )
-    preperation_time = models.IntegerField()
-    picture = models.CharField(max_length=200)
+    path = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    Component = models.CharField(max_length=100)
+    backgroundColor = models.CharField(max_length=100)
+    contentTitle = models.CharField(max_length=100)
+    buttonPath = models.CharField(max_length=100)
+    backgroundImage = models.CharField(max_length=100)
 
+    #title = models.CharField(max_length=200)
+    #description = models.CharField(max_length=1000)
+    #list_of_recipes = ArrayField(
+    #    models.CharField(max_length=200),
+    #    size=20
+    #)
+    #preperation_time = models.IntegerField()
+    #picture = models.CharField(max_length=200)
 
 
