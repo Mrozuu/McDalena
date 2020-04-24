925e8ac1e650dc8902e0b4ff8f7edce70b81e92f
Mrozuu <58520713+Mrozuu@users.noreply.github.com>
2020-04-24 20:47:37 +0200

    MJ #34

---
+++ magdalenabadetko/recipes/models.py
@@ -0,0 +1,20 @@
+from django.contrib.postgres.fields import ArrayField
+from django.db import models
+
+
+class Recipe(models.Model):
+
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=2000)
+    types = models.CharField(max_length=100)
+    picture = models.CharField(max_length=100)
+    #ingredients = ArrayField(
+    #    models.CharField(max_length=100),
+    #    size=30
+    #)
+    #preparation = ArrayField(
+    #    models.CharField(max_length=200),
+    #    size=30
+    #) 
+
+
