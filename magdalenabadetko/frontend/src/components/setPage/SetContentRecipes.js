282e05060b85b51313f4dff039c96874e36976c3
Mrozuu <58520713+Mrozuu@users.noreply.github.com>
2020-05-14 10:47:24 +0200

    MJ #55

--- magdalenabadetko/frontend/src/components/setPage/SetContentRecipes.js
+++ magdalenabadetko/frontend/src/components/setPage/SetContentRecipes.js
@@ -5,18 +5,23 @@ import SetRecipe from "./SetRecipe";
 class SetContentRecipes extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+        instructions: [],
+    };
+
   }
   render() {
     return (
       <div className="setContentRecipes">
         <h2>Przepisy</h2>
-        {this.props.setsData.listOfRecipes.map((item) => (
+        {this.props.setsData.listOfRecipes.map((item) => {
+          console.log(this.props.recipesData[item-1].listOfRecipes);
+          return(
           <SetRecipe
             recipe={this.props.recipesData[item - 1]}
             key={this.props.recipesData[item - 1].id}
-          />
-        ))}
+          />)
+      })}
       </div>
     );
   }
