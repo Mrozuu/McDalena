937116c728802bce4dd27aa832601996e1250f34
Mrozuu <58520713+Mrozuu@users.noreply.github.com>
2020-05-15 09:00:45 +0200

    MJ #59

--- magdalenabadetko/frontend/src/components/setPage/SetContentRecipes.js
+++ magdalenabadetko/frontend/src/components/setPage/SetContentRecipes.js
@@ -13,9 +13,6 @@ class SetContentRecipes extends React.Component {
       <div className="setContentRecipes">
         <h2>Przepisy</h2>
         {this.props.setsData.listOfRecipes.map((item) => {
-<<<<<<< HEAD
-          //          this.props.recipesData[item - 1].listOfElements.map((item2) => {
-          //         });
           return (
             <SetRecipe
               instructions={this.props.instructionsData}
@@ -24,15 +21,6 @@ class SetContentRecipes extends React.Component {
             />
           );
         })}
-=======
-          console.log(this.props.recipesData[item-1].listOfRecipes);
-          return(
-          <SetRecipe
-            recipe={this.props.recipesData[item - 1]}
-            key={this.props.recipesData[item - 1].id}
-          />)
-      })}
->>>>>>> parent of b43a0c4... MJ #56
       </div>
     );
   }
