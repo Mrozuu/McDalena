24a6570e52f54c113fa87cbade8e56dbfd3f7378
kalllllor <karolgren3@gmail.com>
2020-09-02 23:41:52 +0200

    Updates

---
+++ precache-manifest.9a2ea66472d68815df98ea111732f41f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7082f3487ba7cd6681ef7f899c6277a7",
+    "url": "/McDalena/index.html"
+  },
+  {
+    "revision": "1f1b6883511ffe46c934",
+    "url": "/McDalena/static/js/2.9d1bec10.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/McDalena/static/js/2.9d1bec10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0320e1a949cba841cde",
+    "url": "/McDalena/static/js/main.f15efd55.chunk.js"
+  },
+  {
+    "revision": "296728aba85cd5c02ac2",
+    "url": "/McDalena/static/js/runtime-main.217c4bdc.js"
+  },
+  {
+    "revision": "c4b611e6fdb485b2ad56f32f8b535c3b",
+    "url": "/McDalena/static/media/dessert.c4b611e6.svg"
+  },
+  {
+    "revision": "097199bea32aa82886e15e46fdb541c5",
+    "url": "/McDalena/static/media/head.097199be.svg"
+  },
+  {
+    "revision": "6e8bcc82790de60ce413cc5cfbb38fd1",
+    "url": "/McDalena/static/media/mixer.6e8bcc82.svg"
+  },
+  {
+    "revision": "77bfef938fb73036ced30f1d82ca2d2d",
+    "url": "/McDalena/static/media/plate.77bfef93.svg"
+  },
+  {
+    "revision": "42db8d5de7be2e4de9f896c479b2b3b7",
+    "url": "/McDalena/static/media/pot.42db8d5d.svg"
+  },
+  {
+    "revision": "a2867013ac1fbc67c3857be13d2da002",
+    "url": "/McDalena/static/media/set1.a2867013.jpg"
+  },
+  {
+    "revision": "30a3026cf0a4b477ef11585cf3247bb1",
+    "url": "/McDalena/static/media/sign.30a3026c.svg"
+  }
+]);
